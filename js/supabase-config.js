9f46ffd0691b9f7663dd414361a9b21437193437
liuqiufangchocho-jpg <liuqiufang.chocho@gmail.com>
2026-07-14 11:02:59 +0400

    Add files via upload

---
+++ js/supabase-config.js
@@ -0,0 +1,8 @@
+(function (global) {
+  'use strict';
+
+  global.SUPABASE_CONFIG = Object.freeze({
+    url: 'https://hpzjbhjehpxgalslvnqq.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwempiaGplaHB4Z2Fsc2x2bnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMTA4NjUsImV4cCI6MjA5ODg4Njg2NX0.cHSe-udRguaJiPA1oAKwGBA5aFWzpGk_R5dYJsBa404'
+  });
+})(window);
